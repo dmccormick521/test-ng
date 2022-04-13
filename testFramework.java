e8950b7bade6c5567c6df342dbfdbc3f54f22827
dmccormick521 <dmichaelmccormick@gmail.com>
2022-04-13 17:22:02 -0500

    base setup done - need maven project to do more tests

--- testFramework.java
+++ testFramework.java
@@ -1,4 +1,19 @@
 import org.testng.annotations.Test;
 @Test()
 public class testFramework {
+    //base tests
+    @Test(priority = 3)
+    void test1(){
+        System.out.println("I am test1");
+    }
+    @Test(priority = 2)
+    void test2(){
+        System.out.println("I am test2");
+    }
+    @Test(priority = 1)
+    void test3(){
+        System.out.println("I am test3");
+    }
+
+
 }
